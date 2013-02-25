58d3d16ee84bb18c28ef16027af4f19124a08d23
Arthur Langereis <arthur@logic-dream.com>
2013-02-25 10:54:02 -0700

    Initial Commit

---
+++ Quadrae/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Quadrae
+//
+//  Created by Arthur Langereis on 2/25/13.
+//  Copyright (c) 2013 Arthur Langereis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
